25b0b6ecfca97ead9f73aec3da6acda3853e94d5
Nathaniel Bong <nattprog7@gmail.com>
2025-05-27 03:12:19 +0800

    added seeing functionalities to seeingrobot. added nested class to hold locations that robot has seen. Added utility functions to get relative distance. Added sorting function using selection sort to sort locations based on relative distance.

--- oopds-robots/SeeingRobot.cpp
+++ oopds-robots/SeeingRobot.cpp
@@ -8,6 +8,14 @@ SeeingRobot::SeeingRobot()
 SeeingRobot::~SeeingRobot()
 {
     // dtor
+    for (int i = 0; i < view.size(); i++)
+    {
+        if (view[i])
+        {
+            delete view[i];
+        }
+        view[i] = nullptr;
+    }
 }
 
 SeeingRobot::SeeingRobot(const SeeingRobot &other)
@@ -21,4 +29,68 @@ SeeingRobot &SeeingRobot::operator=(const SeeingRobot &rhs)
         return *this; // handle self assignment
     // assignment operator
     return *this;
+}
+
+SeeingRobot::viewLocation::viewLocation(int x, int y, string val)
+{
+    locX = x;
+    locY = y;
+    value = val;
+}
+
+int SeeingRobot::viewRelativeX(viewLocation *loc) const
+{
+    return loc->locX - robotPositionX;
+}
+
+int SeeingRobot::viewRelativeY(viewLocation *loc) const
+{
+    return loc->locY - robotPositionY;
+}
+
+int SeeingRobot::viewRelativeDistance(viewLocation *loc) const
+{
+    int relx = viewRelativeX(loc);
+    int rely = viewRelativeY(loc);
+    if (relx < 0)
+    {
+        relx = -relx;
+    }
+    if (rely < 0)
+    {
+        rely = -rely;
+    }
+    if (relx > rely)
+    {
+        return relx;
+    }
+    else
+    {
+        return rely;
+    }
+}
+
+// selection sort to sort view vector based on closest to furthest
+void SeeingRobot::viewSortView()
+{
+    const int MAX_VIEW = view.size();
+    int minIndex, minValue;
+    viewLocation *temp;
+    for (int start = 0; start < (MAX_VIEW - 1); start++)
+    {
+        minIndex = start;
+        minValue = viewRelativeDistance(view.at(start));
+        for (int index = start + 1; index < MAX_VIEW; index++)
+        {
+            if (viewRelativeDistance(view.at(index)) < minValue)
+            {
+                minValue = viewRelativeDistance(view.at(index));
+                minIndex = index;
+            }
+        }
+        temp = view.at(start);
+        view.at(start) = view.at(minIndex);
+        view.at(minIndex) = temp;
+        temp = nullptr;
+    }
 }
--- oopds-robots/SeeingRobot.h
+++ oopds-robots/SeeingRobot.h
@@ -14,6 +14,32 @@ public:
     virtual void actionLook(Battlefield *battlefield) = 0; // Pure virtual function for looking
 
 protected:
+    int viewColsWidth = -1;
+    int viewRowsWidth = -1;
+    int viewStartCols() { return robotPositionX - 1; }
+    int viewStartRows() { return robotPositionY - 1; }
+    class viewLocation;
+    vector<viewLocation *> view;
+
+    // view actions
+    int viewRelativeX(viewLocation *loc) const;
+
+    int viewRelativeY(viewLocation *loc) const;
+
+    // Chebyshev distance (the max of either X or Y)
+    int viewRelativeDistance(viewLocation *loc) const;
+
+    void viewSortView();
+
+    class viewLocation
+    {
+    public:
+        viewLocation(int x, int, string val);
+        int locX;
+        int locY;
+        string value;
+    };
+
 private:
 };
 
