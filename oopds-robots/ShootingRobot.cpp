51d6730dad6a4c7a0ebac35204b1ec0d52584232
Nathaniel Bong <nattprog7@gmail.com>
2025-06-02 01:49:36 +0800

    corrected the member naming and type in genericrobot

--- oopds-robots/ShootingRobot.cpp
+++ oopds-robots/ShootingRobot.cpp
@@ -1,13 +1,13 @@
-//*********************************************************  
-// Program: YOUR_FILENAME.cpp  
-// Course: CCP6124 OOPDS 
+//*********************************************************
+// Program: YOUR_FILENAME.cpp
+// Course: CCP6124 OOPDS
 // Lecture Class: TC2L
 // Tutorial Class: TT8L
 // Trimester: 2510
 // Member_1: 243UC247CZ | ABDULLAH HAKEEM BIN AHMAD KAMAL | ABDULLAH.HAKEEM.AHMAD@student.mmu.edu.my | +6012-4188-300
-// Member_2: 242UC244GD | YOSHENAN A/L SHANKER | YOSHENAN .SHANKER@student.mmu.edu.my | PHONE
+// Member_2: 242UC244GD | BONG WEI QIANG, NATHANIEL | BONG.WEI.QIANG@student.mmu.edu.my | +6011-6111-2440
 // Member_3: 243UC2463Z | PRABU NATAR A/L DHARMENDRA | PRABU.NATAR.DHARMENDRA@student.mmu.edu.my | PHONE
-// Member_4: 243UC247BM | BONG WEI QIANG, NATHANIEL | BONG.WEI.QIANG@student.mmu.edu.my | +6011-6111-2440	 
+// Member_4: 243UC247BM | YOSHENAN A/L SHANKER | YOSHENAN .SHANKER@student.mmu.edu.my | PHONE
 //*********************************************************
 // Task Distribution
 // Member_1: Read from file input. Parse file input as battlefield information, number of robots and robot values (type, id, location).
